af5c55c22fdd248c87458c703a12e49dc6275879
cknoettg <cknoettg@yahoo.com>
2020-04-17 21:43:56 -0400

    Added skeleton structure to App and Character.

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,12 @@
 // Write your Character component here
+import React, { useState } from 'react';
+
+
+const Characters = props => {
+
+    const [character, setCharacter] = useState([]);
+
+    
+}
+
+export default Character;
